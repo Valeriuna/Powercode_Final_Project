401b6ccae4ef12629869d9542fb4b3a421af27b7
Valeriuna <106536158+Valeriuna@users.noreply.github.com>
2023-01-22 20:21:39 +0200

    add array

--- index.js
+++ index.js
@@ -19,3 +19,41 @@ const generalClose = document.addEventListener("click", function (event) {
     closeModal();
   }
 });
+
+
+
+const stepsArr = [
+  {
+    name: "Date",
+    text: "choose what date <br /> to check",
+    imgUrl: "img/healthcare 1.png",
+    imgDescription: "healthcare",
+  },
+  {
+    name: "Poly",
+    text: "choose what Poly <br /> to check",
+    imgUrl: "img/healthcare 2.png",
+    imgDescription: "healthcare",
+  },
+  {
+    name: "Doctor",
+    text: "And choose doctor <br /> to check",
+    imgUrl: "img/doctor 2.png",
+    imgDescription: "doctor",
+  },
+];
+  const steps = document.getElementById("steps");
+
+ function getHtmlFromSteps(step) {
+    return `<div class="item"><img src="${step.imgUrl}" alt="${step.imgDescription}" /><h3>${step.name}</h3><p>${step.text}</p></div>`;
+ }
+
+let html = "";
+
+for (let i = 0; i < stepsArr.length; i++) {
+   html += getHtmlFromSteps(stepsArr[i]);
+}
+
+ steps.innerHTML = html;
+
+
